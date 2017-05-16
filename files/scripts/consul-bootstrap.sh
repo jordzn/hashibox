46771bae1bfbbbb7f61040452252d536da03b387
jordzn <jordan.taylor@contino.io>
2017-05-16 11:59:35 +0100

    demo ready

--- files/scripts/consul-bootstrap.sh
+++ files/scripts/consul-bootstrap.sh
@@ -6,17 +6,24 @@ set -euo pipefail
 # 2 = node name
 # 3 = bind address
 # 4 = cluster size // agent target server
-# 5 - n = consul server members
+# 5 = is initial server - true/false
+# 6 - n = consul server members
+
 
 yum install unzip bind-utils -y
-unzip /tmp/synced_folder/consul_0.7.1_linux_amd64.zip -d /tmp
+unzip /tmp/synced_folder/consul_0.8.3_linux_amd64.zip -d /tmp
 sudo mv /tmp/consul /usr/bin
 sudo mkdir -p /etc/consul.d
 
 if $1; then
-  consul agent -server -data-dir=/tmp/consul -node=$2 -bind=$3 -bootstrap-expect=$4 -config-dir=/etc/consul.d -ui -client 0.0.0.0 &>/tmp/logs/$2.log &
-  sleep 5
-  consul join $5 $6 $7
+  if $5; then
+    consul agent -server -data-dir=/tmp/consul -node=$2 -bind=$3 -bootstrap-expect=$4 -config-dir=/etc/consul.d -ui -client 0.0.0.0 &>/tmp/logs/$2.log &
+    sleep 5
+  else
+    consul agent -server -data-dir=/tmp/consul -node=$2 -bind=$3 -bootstrap-expect=$4 -config-dir=/etc/consul.d -ui -client 0.0.0.0 &>/tmp/logs/$2.log &
+    sleep 5
+    consul join $6
+  fi
 else
   consul agent -data-dir=/tmp/consul -node=$2 -bind=$3 -config-dir=/etc/consul.d &>/dev/null &
   sleep 5
--- files/scripts/vault-bootstrap.sh
+++ files/scripts/vault-bootstrap.sh
@@ -8,7 +8,7 @@ sudo yum install unzip bind-utils wget -y
 wget -O jq https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64
 sudo chmod +x ./jq
 sudo cp jq /usr/bin
-unzip /tmp/synced_folder/vault_0.6.2_linux_amd64.zip -d /tmp
+unzip /tmp/synced_folder/vault_0.7.2_linux_amd64.zip -d /tmp
 sudo mkdir /etc/vault
 sudo mv /tmp/vault /usr/bin
 sudo cp /tmp/config/vault/vault.hcl /etc/vault/vault.hcl
@@ -18,13 +18,13 @@ export VAULT_ADDR=http://0.0.0.0:8200
 echo "export VAULT_ADDR=http://0.0.0.0:8200" >> ~/.bash_profile
 sudo vault server -config=/etc/vault/vault.hcl &>/tmp/logs/vault-$1.log &
 
-if [[ $1 == 0 && ! -f /tmp/config/vault/keys ]]; then
-  vault init -key-shares=5 -key-threshold=3 >> /tmp/config/vault/keys || true
-fi
+sudo mkdir -p /etc/consul.d
+sudo cp /tmp/config/vault/vault-leader.json /etc/consul.d/vault-leader.json
 
+# if [[ $1 == 0 && ! -f /tmp/config/vault/keys ]]; then
+#   vault init -key-shares=5 -key-threshold=3 >> /tmp/config/vault/keys || true
+# fi
+#
 for (( i = 1; i < 4; i++ )); do
   KEY=`grep "Unseal Key $i:" /tmp/config/vault/keys | cut -c 15-` && vault unseal $KEY -address=http://0.0.0.0:8200
 done
-
-sudo mkdir -p /etc/consul.d
-sudo cp /tmp/config/vault/vault-leader.json /etc/consul.d/vault-leader.json
